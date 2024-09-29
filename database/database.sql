eb88fee422f3cbd8214b815b28512c9d65671a0f
Osiel-RA <o.rocha20@info.uas.edu.mx>
2024-09-28 18:10:03 -0700

    OR - Update initial script to database - News Tables

--- database/database.sql
+++ database/database.sql
@@ -68,16 +68,48 @@ CREATE TABLE card (
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
-CREATE TABLE user (
-    id SERIAL PRIMARY KEY,
-    name VARCHAR(50),
-    username VARCHAR(255),
-    password VARCHAR(255),
+CREATE TABLE password_reset_token (
+    username VARCHAR(255) PRIMARY KEY,
+    token VARCHAR(255) NOT NULL,
+    created_at TIMESTAMP NULL
+);
+
+CREATE TABLE "user" (
+    id SERIAL PRIMARY KEY,  
+    name VARCHAR(50) NOT NULL,
+    username VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL,
     phone CHAR(10),
-    status_id INT,
+    username_verified_at TIMESTAMP NULL,
+    status_id BIGINT NOT NULL,
+    remember_token VARCHAR(100),
     created_at DATE DEFAULT CURRENT_DATE,
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     CONSTRAINT fk_status FOREIGN KEY (status_id) REFERENCES status(id)
+);
+
+CREATE TABLE session (
+    id VARCHAR(255) PRIMARY KEY,
+    user_id INT NULL,
+    ip_address VARCHAR(45) NULL,
+    user_agent TEXT NULL,
+    payload TEXT NOT NULL,
+    last_activity INTEGER NOT NULL,
+    CONSTRAINT fk_user FOREIGN KEY (user_id) REFERENCES "user"(id)
+);
+
+CREATE TABLE personal_access_token (
+    id SERIAL PRIMARY KEY,
+    tokenable_id BIGINT NOT NULL,
+    tokenable_type VARCHAR(255) NOT NULL,
+    name VARCHAR(255) NOT NULL,
+    token VARCHAR(64) UNIQUE NOT NULL,
+    abilities TEXT NULL,
+    last_used_at TIMESTAMP NULL,
+    expires_at TIMESTAMP NULL,
+    created_at DATE DEFAULT CURRENT_DATE,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    CONSTRAINT fk_tokenable FOREIGN KEY (tokenable_id) REFERENCES "user"(id)
 );
 
 CREATE TABLE profile (
@@ -90,7 +122,7 @@ CREATE TABLE profile (
     created_at DATE DEFAULT CURRENT_DATE,
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     CONSTRAINT fk_language FOREIGN KEY (language_id) REFERENCES language(id),
-    CONSTRAINT fk_user FOREIGN KEY (user_id) REFERENCES user(id),
+    CONSTRAINT fk_user FOREIGN KEY (user_id) REFERENCES "user"(id),
     CONSTRAINT fk_classification FOREIGN KEY (classification_id) REFERENCES classification(id),
     CONSTRAINT fk_status FOREIGN KEY (status_id) REFERENCES status(id)
 );
@@ -148,7 +180,7 @@ CREATE TABLE payment (
     plan_type_id INT,
     created_at DATE DEFAULT CURRENT_DATE,
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    CONSTRAINT fk_user FOREIGN KEY (user_id) REFERENCES user(id),
+    CONSTRAINT fk_user FOREIGN KEY (user_id) REFERENCES "user"(id),
     CONSTRAINT fk_pay_method FOREIGN KEY (pay_method_id) REFERENCES pay_method(id),
     CONSTRAINT fk_plan_type FOREIGN KEY (plan_type_id) REFERENCES plan_type(id)
 );
@@ -163,6 +195,6 @@ CREATE TABLE user_card (
     expiration_date DATE,
     created_at DATE DEFAULT CURRENT_DATE,
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    CONSTRAINT fk_user FOREIGN KEY (user_id) REFERENCES user(id),
+    CONSTRAINT fk_user FOREIGN KEY (user_id) REFERENCES "user"(id),
     CONSTRAINT fk_card FOREIGN KEY (card_id) REFERENCES card(id)
 );
